0f8ac559401e40f236153c5b4ca52f742e576f54
abramscris <cris-dias@hotmail.com>
2022-04-17 17:39:51 -0400

    update

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,53 +1,64 @@
-SELECT e.emp_no,
-		e.first_name,
-		e.last_name,
-		t.title,
-		t.from_date,
-		t.to_date
-
+SELECT employees.emp_no,
+	employees.first_name,
+    employees.last_name,
+	titles.title,
+    titles.from_date,
+    titles.to_date
 INTO retirement_titles
-FROM employees AS e
-	INNER JOIN titles AS t
-		ON (e.emp_no = t.emp_no)
-WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-ORDER BY emp_no ASC;
-
-SELECT * FROM retirees_titles;
+FROM employees
+LEFT JOIN titles
+ON employees.emp_no = titles.emp_no
+WHERE employees.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+ORDER BY employees.emp_no
+SELECT * FROM retirement_titles
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
-rt.first_name,
-rt.last_name,
-rt.title
-
+SELECT DISTINCT ON (retirement_titles.emp_no) retirement_titles.emp_no,
+	retirement_titles.first_name,
+    retirement_titles.last_name,
+	retirement_titles.title
 INTO unique_titles
-FROM retirement_titles AS rt
-ORDER BY emp_no, to_date DESC;
+FROM retirement_titles
+ORDER BY retirement_titles.emp_no, retirement_titles.to_date DESC;
+SELECT * FROM unique_titles
 
---Table 3
-SELECT COUNT(ut.emp_no), ut.title
+
+SELECT title, COUNT(*)
 INTO retiring_titles
-FROM unique_titles as ut
+FROM unique_titles
 GROUP BY title
-ORDER BY COUNT(title) DESC;
+ORDER BY 2 DESC
+SELECT * FROM retiring_titles
 
---Deliverable 2
---Mentorship Eligibility table
+SELECT DISTINCT ON (employees.emp_no) employees.emp_no,
+	employees.first_name,
+	employees.last_name,
+	employees.birth_date,
+	dept_emp.from_date,
+	dept_emp.to_date,
+	titles.title
+INTO mentorship_eligibility
+FROM employees
+LEFT JOIN dept_emp
+ON employees.emp_no = dept_emp.emp_no
+LEFT JOIN titles
+ON titles.emp_no = employees.emp_no
+WHERE employees.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+ORDER BY employees.emp_no
+SELECT * FROM mentorship_eligibility
 
-SELECT DISTINCT ON(e.emp_no)e.emp_no,
-	e.first_name,
-	e.last_name,
-	e.birth_date,
-	de.from_date,
-	de.to_date,
-	t.title
---DISTINCT ON(emp_no)
-INTO mentorship_table
-FROM employees AS e
-	INNER JOIN dept_emp AS de
-		ON (e.emp_no = de.emp_no)
-			INNER JOIN titles AS t
-				ON (e.emp_no = t.emp_no)
-WHERE (e.birth_date BETWEEN '1962-01-01' AND '1965-12-31')
-AND (de.to_date = '9999-01-01')
-ORDER BY emp_no;
+-- Number of total employees
+SELECT DISTINCT ON (employees.emp_no) employees.emp_no,
+    employees.first_name,
+    employees.last_name,
+    titles.title,
+    titles.from_date,
+    titles.to_date
+INTO all_employees
+FROM employees
+LEFT JOIN titles
+ON employees.emp_no = titles.emp_no
+ORDER BY employees.emp_no, titles.to_date DESC
+
+SELECT COUNT (emp_no)
+FROM all_employees
